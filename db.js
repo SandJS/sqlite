a9675eeb85746e79f68406552525ae41026f6939
Adam Jaso <mkltwain@gmail.com>
2016-05-12 20:36:57 -0700

    updated to use database wrapper

---
+++ db.js
@@ -0,0 +1,93 @@
+"use strict";
+
+class DB {
+  constructor(db, config) {
+    this.db = db;
+    this.config = config;
+  }
+
+  query(query, params) {
+    return new Promise((resolve, reject) => {
+
+      if (/^select/i.test(query)) {
+        this.db.all(query, params, (err, rows) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(rows);
+        });
+
+      } else {
+        this.db.run(query, params, function(err) {
+          if (err) {
+            return reject(err);
+          }
+          resolve(this);
+        });
+      }
+    });
+  }
+
+  selectOne(query, params) {
+    return new Promise((resolve, reject) => {
+      this.db.get(query, params, (err, row) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(row);
+      });
+    })
+  }
+
+  close() {
+    return new Promise((resolve, reject) => {
+      this.db.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    })
+  }
+
+  static newInstance(config) {
+    config = config || {};
+    return new Promise((resolve, reject) => {
+      let db;
+
+      let filename = config.filename;
+      let mode = config.mode;
+      let init = config.init;
+
+      if (filename) {
+        db = new sqlite3.Database(filename, callback);
+
+      } else if (filename && 'undefined' !== typeof mode) {
+        db = new sqlite3.Database(filename, mode, callback);
+
+      } else {
+        throw new Error('Invalid config');
+      }
+
+      db = new DB(db);
+
+      function callback(err) {
+        if (err) {
+          return reject(err);
+        }
+        if ('function' == typeof init) {
+          init(db, (err) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(db);
+          });
+        } else {
+          resolve(db);
+        }
+      }
+    });
+  }
+}
+
+module.exports = DB;
--- sqlite.js
+++ sqlite.js
@@ -2,6 +2,8 @@
 
 const SandGrain = require('sand-grain');
 const sqlite3 = require('sqlite3');
+const _ = require('lodash');
+const DB = require('./db');
 
 class SQLite extends SandGrain {
 
@@ -30,80 +32,27 @@ class SQLite extends SandGrain {
     if (!this.db) {
       done();
     } else {
-      this.db.close(done);
+      this.db.close()
+        .then(done)
+        .catch(done);
     }
   }
 
   query(query, params) {
-    return new Promise((resolve, reject) => {
-
-      if (/^select/i.test(query)) {
-        this.db.all(query, params, (err, rows) => {
-          if (err) {
-            return reject(err);
-          }
-          resolve(rows);
-        });
-
-      } else {
-        this.db.run(query, params, function(err) {
-          if (err) {
-            return reject(err);
-          }
-          resolve(this);
-        });
-      }
-    });
+    return this.db.query(query, params);
   }
 
   selectOne(query, params) {
-    return new Promise((resolve, reject) => {
-      this.db.get(query, params, (err, row) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(row);
-      });
-    })
+    return this.db.selectOne(query, params);
   }
 
   newClient(config) {
-    config = config || {};
-    return new Promise((resolve, reject) => {
-      let db;
-
-      let filename = config.filename || this.config.filename;
-      let mode = config.mode || this.config.mode;
-      let init = config.init || this.config.init;
-
-      if (filename) {
-        db = new sqlite3.Database(filename, callback);
-
-      } else if (filename && 'undefined' !== typeof mode) {
-        db = new sqlite3.Database(filename, mode, callback);
-
-      } else {
-        throw new Error('Invalid config');
-      }
-
-      function callback(err) {
-        if (err) {
-          return reject(err);
-        }
-        if ('function' == typeof init) {
-          init(db, (err) => {
-            if (err) {
-              return reject(err);
-            }
-            resolve(db);
-          });
-        } else {
-          resolve(db);
-        }
-      }
-    });
+    config = _.defaults({}, config, this.config);
+    return DB.newInstance(config);
   }
 
 }
 
 module.exports = SQLite;
+
+SQLite.DB = DB;
